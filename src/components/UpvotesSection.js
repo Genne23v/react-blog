0634b170b4b3137c8efb766b1034ef6ff08eb7f5
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-10-30 21:00:39 -0400

    Blog front-end complete

---
+++ src/components/UpvotesSection.js
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+
+const UpvotesSection = ({ articleName, upvotes, setArticleInfo }) => {
+    const upvoteArticle = async () => {
+        const result = await fetch(
+            `http://localhost:8000/api/articles/${articleName}/upvote`,
+            {
+                method: 'post',
+            },
+        );
+        console.log(result);
+        const body = await result.json();
+
+        setArticleInfo(body);
+    };
+    return (
+        <div id="upvotes-section">
+            <button onClick={() => upvoteArticle()}>Add Upvote</button>
+            <p>This post has been upvoted {upvotes} times</p>
+        </div>
+    );
+};
+
+export default UpvotesSection;
